8a9e35ecf8f67a5ce338807a8c26fb8c54b36fe6
yeop12 <duq47@naver.com>
2023-02-01 21:18:26 +0900

    SheetInfo의 일부를 SheetInfo처럼 사용할 수 있는 SheetInfoView class추가

---
+++ SheetDataTool/SheetInfoView.cs
@@ -0,0 +1,27 @@
+﻿namespace SheetDataTool
+{
+	internal class SheetInfoView
+	{
+		private readonly SheetInfo _sheetInfo;
+		private readonly int _startRow;
+		private readonly int _startColumn;
+
+		public string? this[int row, int column] => _sheetInfo[row + _startRow, column + _startColumn];
+		public int RowCount { get; }
+		public int ColumnCount { get; }
+
+		public SheetInfoView( SheetInfo sheetInfo, int startRow, int startColumn, int endRow, int endColumn )
+		{
+			if (startRow < 0 || startRow >= sheetInfo.RowCount) throw new ArgumentOutOfRangeException(nameof(startRow));
+			if(startColumn < 0 || startColumn >= sheetInfo.ColumnCount) throw new ArgumentOutOfRangeException(nameof(startColumn));
+			if(endRow < 0 || endRow >= sheetInfo.RowCount) throw new ArgumentOutOfRangeException(nameof(endRow));
+			if(endColumn < 0 || endColumn >= sheetInfo.ColumnCount) throw new ArgumentOutOfRangeException(nameof(endColumn));
+			
+			_sheetInfo = sheetInfo;
+			_startRow = startRow;
+			_startColumn = startColumn;
+			RowCount = endRow - startRow + 1;
+			ColumnCount = endColumn - startColumn + 1;
+		}
+	}
+}
