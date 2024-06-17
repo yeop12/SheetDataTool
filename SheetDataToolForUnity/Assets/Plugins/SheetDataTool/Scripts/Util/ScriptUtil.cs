21de3cf943db97b0180daa91697eade1507af44b
yeop12 <duq47@naver.com>
2024-06-17 20:28:55 +0900

    비동기 함수 및 전체 로딩 함수 추가

--- SheetDataToolForUnity/Assets/Plugins/SheetDataTool/Scripts/Util/ScriptUtil.cs
+++ SheetDataToolForUnity/Assets/Plugins/SheetDataTool/Scripts/Util/ScriptUtil.cs
@@ -1,37 +1,111 @@
 ﻿using System;
+using System.Linq;
 
 namespace SheetDataTool
 {
 	public static class ScriptUtil
 	{
+		private static string ToFunctionName(this string name, Setting setting) =>
+			name.ChangeNotation(Notation.Pascal, setting.ScriptFunctionNameNotation);
+
 		public static string GetBaseClassName(Setting setting) => "BaseSheetDataHelper".ChangeNotation(Notation.Pascal, setting.ScriptClassNameNotation);
 
 		public static string GetBaseClassScript(Setting setting)
 		{
 			var sb = new ScopedStringBuilder();
+			sb.WriteLine("using System.Linq;");
+			sb.WriteLine("using System.Reflection;");
+			sb.WriteLine("using System.Threading.Tasks;");
+			foreach (var platformInfo in setting.PlatformInfos)
+			{
+				sb.WriteLine($"#if {platformInfo.DefineName}");
+				platformInfo.NamespaceNames.ForEach(x => sb.WriteLine($"using {x};"));
+				sb.WriteLine("#endif");
+			}
+			sb.WriteLine();
 
 			var namespaceScope = string.IsNullOrWhiteSpace(setting.NamespaceName)
 				? null : sb.StartScope($"namespace {setting.NamespaceName}");
 
 			using (sb.StartScope($"public abstract record {GetBaseClassName(setting)}"))
 			{
-				var isFirst = true;
-				foreach (var platformInfo in setting.PlatformInfos)
+				foreach (var (platformInfo, index) in setting.PlatformInfos.Select((x, i) => (x, i)))
 				{
-					if (isFirst)
-					{
-						sb.WriteLine($"#if {platformInfo.DefineName}");
-						isFirst = false;
-					}
-					else
-					{
-						sb.WriteLine($"#elif {platformInfo.DefineName}");
-					}
+					sb.WriteLine($"{(index == 0 ? "#if" : "#elif")} {platformInfo.DefineName}");
 					sb.WriteLine($"protected static readonly string DefaultDirectory = \"{platformInfo.DefaultDirectory}\";");
 				}
 				sb.WriteLine("#else");
 				sb.WriteLine($"protected static readonly string DefaultDirectory = string.Empty;");
 				sb.WriteLine("#endif");
+				sb.WriteLine();
+				
+				using (sb.StartScope($"protected static string {"ReadData".ToFunctionName(setting)}<T>()"))
+				{
+					foreach (var (settingPlatformInfo, index) in setting.PlatformInfos.Select((x, i) => (x, i)))
+					{
+						sb.WriteLine($"{(index == 0 ? "#if" : "#elif")} {settingPlatformInfo.DefineName}");
+
+						switch (settingPlatformInfo.Platform)
+						{
+							case Platform.Unity:
+								sb.WriteLine("var textAsset = Addressables.LoadAssetAsync<TextAsset>($\"{DefaultDirectory}/{typeof(T).Name}\").WaitForCompletion();");
+								sb.WriteLine("var json = textAsset.text;");
+								break;
+
+							case Platform.CSharp:
+								sb.WriteLine("var json = System.IO.File.ReadAllText($\"{DefaultDirectory}/{typeof(T).Name}.json\");");
+								break;
+
+							default:
+								throw new NotImplementedException($"{settingPlatformInfo.Platform}");
+						}
+					}
+					sb.WriteLine("#else");
+					sb.WriteLine("var json = string.Empty;");
+					sb.WriteLine("#endif");
+					sb.WriteLine();
+
+					sb.WriteLine("return json;");
+				}
+				sb.WriteLine();
+				
+				using (sb.StartScope($"protected static async Task<string> {"ReadDataAsync".ToFunctionName(setting)}<T>()"))
+				{
+					foreach (var (settingPlatformInfo, index) in setting.PlatformInfos.Select((x, i) => (x, i)))
+					{
+						sb.WriteLine($"{(index == 0 ? "#if" : "#elif")} {settingPlatformInfo.DefineName}");
+
+						switch (settingPlatformInfo.Platform)
+						{
+							case Platform.Unity:
+								sb.WriteLine("var textAsset = await Addressables.LoadAssetAsync<TextAsset>($\"{DefaultDirectory}/{typeof(T).Name}\").Task;");
+								sb.WriteLine("var json = textAsset.text;");
+								break;
+
+							case Platform.CSharp:
+								sb.WriteLine("var json = await System.IO.File.ReadAllTextAsync($\"{DefaultDirectory}/{typeof(T).Name}.json\");");
+								break;
+
+							default:
+								throw new NotImplementedException($"{settingPlatformInfo.Platform}");
+						}
+					}
+					sb.WriteLine("#else");
+					sb.WriteLine("var json = string.Empty;");
+					sb.WriteLine("#endif");
+					sb.WriteLine();
+
+					sb.WriteLine("return json;");
+				}
+				sb.WriteLine();
+
+				using (sb.StartScope($"public static async Task {"LoadAllDataAsync".ToFunctionName(setting)}()"))
+				{
+					sb.WriteLine($"var sheetTypes = typeof({GetBaseClassName(setting)}).Assembly.GetTypes().Where(x =>x.IsAbstract is false && x.IsSubclassOf(typeof({GetBaseClassName(setting)})));");
+					sb.WriteLine($"var methodInfos = sheetTypes.Select(x => x.GetMethod(\"{"LoadDataAsync".ToFunctionName(setting)}\", BindingFlags.Static | BindingFlags.FlattenHierarchy | BindingFlags.Public));");
+					sb.WriteLine("var loadFunctions = methodInfos.Select(x => x?.Invoke(null, new object[] { })).Cast<Task>();");
+					sb.WriteLine("await Task.WhenAll(loadFunctions);");
+				}
 			}
 
 			namespaceScope?.Dispose();
@@ -64,13 +138,8 @@ namespace SheetDataTool
 			sb.WriteLine("using System;");
 			sb.WriteLine("using System.Collections.Generic;");
 			sb.WriteLine("using System.Linq;");
+			sb.WriteLine("using System.Threading.Tasks;");
 			sb.WriteLine("using Newtonsoft.Json;");
-			foreach (var platformInfo in setting.PlatformInfos)
-			{
-				sb.WriteLine($"#if {platformInfo.DefineName}");
-				platformInfo.NamespaceNames.ForEach(x => sb.WriteLine($"using {x};"));
-				sb.WriteLine("#endif");
-			}
 			sb.WriteLine();
 
 			var namespaceScope = string.IsNullOrWhiteSpace(setting.NamespaceName)
@@ -78,53 +147,40 @@ namespace SheetDataTool
 
 			using (sb.StartScope($"public abstract record {GetDesignClassName(setting)}<TKey, TValue> : {GetBaseClassName(setting)} where TValue : {GetDesignInterfaceName(setting)}<TKey>"))
 			{
-				var loadDataFunctionName =
-					"LoadData".ChangeNotation(Notation.Pascal, setting.ScriptFunctionNameNotation);
 				var dataPrivateName = $"{setting.ScriptPrivateVariableNamePrefix}{"data".ChangeNotation(Notation.Camel, setting.ScriptPrivateVariableNameNotation)}";
-				sb.WriteLine($"private static readonly Lazy<Dictionary<TKey, TValue>> {dataPrivateName} = new({loadDataFunctionName});");
+				sb.WriteLine($"private static Lazy<Dictionary<TKey, TValue>> {dataPrivateName} = new({"LoadData".ToFunctionName(setting)});");
 				sb.WriteLine();
+
 				sb.WriteLine($"public static IEnumerable<TValue> {"Data".ChangeNotation(Notation.Pascal, setting.ScriptPublicVariableNameNotation)} => {dataPrivateName}.Value.Values;");
 				sb.WriteLine();
-				using (sb.StartScope($"private static Dictionary<TKey, TValue> {loadDataFunctionName}()"))
+
+				using (sb.StartScope($"private static Dictionary<TKey, TValue> {"LoadData".ToFunctionName(setting)}()"))
 				{
 					var keyName = "Key".ChangeNotation(Notation.Pascal, setting.ScriptPublicVariableNameNotation);
-					var isFirst = true;
-					foreach (var settingPlatformInfo in setting.PlatformInfos)
-					{
-						if (isFirst)
-						{
-							sb.WriteLine($"#if {settingPlatformInfo.DefineName}");
-							isFirst = false;
-						}
-						else
-						{
-							sb.WriteLine($"#elif {settingPlatformInfo.DefineName}");
-						}
-
-						switch (settingPlatformInfo.Platform)
-						{
-							case Platform.Unity:
-								sb.WriteLine("var textAsset = Addressables.LoadAssetAsync<TextAsset>($\"{DefaultDirectory}/{typeof(TValue).Name}\").WaitForCompletion();");
-								sb.WriteLine("var json = textAsset.text;");
-								break;
-
-							case Platform.CSharp:
-								sb.WriteLine("var json = System.IO.File.ReadAllText($\"{DefaultDirectory}/{typeof(TValue).Name}.json\");");
-								break;
-
-							default:
-								throw new NotImplementedException($"{settingPlatformInfo.Platform}");
-						}
-					}
-					sb.WriteLine("#else");
-					sb.WriteLine("var json = string.Empty;");
-					sb.WriteLine("#endif");
+					sb.WriteLine($"var json = {"ReadData".ToFunctionName(setting)}<TValue>();");
 					sb.WriteLine();
 					using (sb.StartScope("if (JsonConvert.DeserializeObject<List<TValue>>(json) is not { } data)")) 
 					{
 						sb.WriteLine("throw new Exception($\"{typeof(TValue).Name} does not loaded.\");");
 					}
 					sb.WriteLine($"return data.ToDictionary(x => x.{keyName}, x => x);");
+				}
+				sb.WriteLine();
+				
+				using (sb.StartScope($"public static async Task {"LoadDataAsync".ToFunctionName(setting)}()"))
+				{
+					var keyName = "Key".ChangeNotation(Notation.Pascal, setting.ScriptPublicVariableNameNotation);
+					sb.WriteLine($"var json = await {"ReadDataAsync".ToFunctionName(setting)}<TValue>();");
+					sb.WriteLine();
+
+					using (sb.StartScope("if (JsonConvert.DeserializeObject<List<TValue>>(json) is not { } data)")) 
+					{
+						sb.WriteLine("throw new Exception($\"{typeof(TValue).Name} does not loaded.\");");
+					}
+					sb.WriteLine();
+
+					sb.WriteLine($"var dataDictionary = data.ToDictionary(x => x.{keyName}, x => x);");
+					sb.WriteLine($"{dataPrivateName} = new Lazy<Dictionary<TKey, TValue>>(dataDictionary);");
 				}
 				sb.WriteLine();
 
@@ -162,13 +218,8 @@ namespace SheetDataTool
 		{
 			var sb = new ScopedStringBuilder();
 			sb.WriteLine("using System;");
+			sb.WriteLine("using System.Threading.Tasks;");
 			sb.WriteLine("using Newtonsoft.Json;");
-			foreach (var platformInfo in setting.PlatformInfos)
-			{
-				sb.WriteLine($"#if {platformInfo.DefineName}");
-				platformInfo.NamespaceNames.ForEach(x => sb.WriteLine($"using {x};"));
-				sb.WriteLine("#endif");
-			}
 			sb.WriteLine();
 
 			var namespaceScope = string.IsNullOrWhiteSpace(setting.NamespaceName)
@@ -178,44 +229,22 @@ namespace SheetDataTool
 			{
 				sb.WriteLine("protected static bool IsLoaded;");
 				sb.WriteLine();
-
-				var loadDataFunctionName =
-					"LoadData".ChangeNotation(Notation.Pascal, setting.ScriptFunctionNameNotation);
-				using (sb.StartScope($"public static void {loadDataFunctionName}()"))
+				
+				using (sb.StartScope($"protected static void {"LoadData".ToFunctionName(setting)}()"))
 				{
 					sb.WriteLine("IsLoaded = true;");
-					var isFirst = true;
-					foreach (var platformInfo in setting.PlatformInfos)
-					{
-						if (isFirst)
-						{
-							sb.WriteLine($"#if {platformInfo.DefineName}");
-							isFirst = false;
-						}
-						else
-						{
-							sb.WriteLine($"#elif {platformInfo.DefineName}");
-						}
+					sb.WriteLine($"var json = {"ReadData".ToFunctionName(setting)}<T>();");
+					sb.WriteLine();
 
-						switch (platformInfo.Platform)
-						{
-							case Platform.Unity:
-								sb.WriteLine("var textAsset = Addressables.LoadAssetAsync<TextAsset>($\"{DefaultDirectory}/{typeof(T).Name}\").WaitForCompletion();");
-								sb.WriteLine("var json = textAsset.text;");
-								break;
+					sb.WriteLine("if (JsonConvert.DeserializeObject<T>(json) is { }) return;");
+					sb.WriteLine("throw new Exception($\"{ typeof(T).Name} does not loaded.\");");
+				}
+				sb.WriteLine();
 
-							case Platform.CSharp:
-								sb.WriteLine("var json = System.IO.File.ReadAllText($\"{DefaultDirectory}/{typeof(T).Name}.json\");");
-								break;
-
-							default:
-								throw new NotImplementedException($"{platformInfo.Platform}");
-						}
-						
-					}
-					sb.WriteLine("#else");
-					sb.WriteLine("var json = string.Empty;");
-					sb.WriteLine("#endif");
+				using (sb.StartScope($"public static async Task {"LoadDataAsync".ToFunctionName(setting)}()"))
+				{
+					sb.WriteLine("IsLoaded = true;");
+					sb.WriteLine($"var json = await {"ReadDataAsync".ToFunctionName(setting)}<T>();");
 					sb.WriteLine();
 
 					sb.WriteLine("if (JsonConvert.DeserializeObject<T>(json) is { }) return;");
@@ -235,13 +264,8 @@ namespace SheetDataTool
 			sb.WriteLine("using System;");
 			sb.WriteLine("using System.Collections.Generic;");
 			sb.WriteLine("using System.Linq;");
+			sb.WriteLine("using System.Threading.Tasks;");
 			sb.WriteLine("using Newtonsoft.Json;");
-			foreach (var platformInfo in setting.PlatformInfos)
-			{
-				sb.WriteLine($"#if {platformInfo.DefineName}");
-				platformInfo.NamespaceNames.ForEach(x => sb.WriteLine($"using {x};"));
-				sb.WriteLine("#endif");
-			}
 			sb.WriteLine();
 
 			var namespaceScope = string.IsNullOrWhiteSpace(setting.NamespaceName)
@@ -253,7 +277,7 @@ namespace SheetDataTool
 				var dataPrivateName = $"{setting.ScriptPrivateVariableNamePrefix}{"data".ChangeNotation(Notation.Camel, setting.ScriptPrivateVariableNameNotation)}";
 				sb.WriteLine($"private static Dictionary<TKey, TValue> {dataPrivateName};");
 				var dataPublicName = "Data".ChangeNotation(Notation.Pascal, setting.ScriptPublicVariableNameNotation);
-				var loadDataFunctionName = "LoadData".ChangeNotation(Notation.Pascal, setting.ScriptFunctionNameNotation);
+				var loadDataFunctionName = "LoadData".ToFunctionName(setting);
 				using (sb.StartScope($"public static IEnumerable<TValue> {dataPublicName}"))
 				{
 					using (sb.StartScope("get"))
@@ -267,38 +291,24 @@ namespace SheetDataTool
 				using (sb.StartScope($"protected static void {loadDataFunctionName}()"))
 				{
 					sb.WriteLine("IsLoaded = true;");
-					var isFirst = true;
-					foreach (var platformInfo in setting.PlatformInfos)
+					sb.WriteLine($"var json = {"ReadData".ToFunctionName(setting)}<TValue>();");
+					sb.WriteLine();
+
+					sb.WriteLine("var (_ ,data) = JsonConvert.DeserializeObject<(TValue, List<TValue>)>(json);");
+					using (sb.StartScope("if(data is null)"))
 					{
-						if (isFirst)
-						{
-							sb.WriteLine($"#if {platformInfo.DefineName}");
-							isFirst = false;
-						}
-						else
-						{
-							sb.WriteLine($"#elif {platformInfo.DefineName}");
-						}
-
-						switch (platformInfo.Platform)
-						{
-							case Platform.Unity:
-								sb.WriteLine("var textAsset = Addressables.LoadAssetAsync<TextAsset>($\"{DefaultDirectory}/{typeof(TValue).Name}\").WaitForCompletion();");
-								sb.WriteLine("var json = textAsset.text;");
-								break;
-
-							case Platform.CSharp:
-								sb.WriteLine("var json = System.IO.File.ReadAllText($\"{DefaultDirectory}/{typeof(TValue).Name}.json\");");
-								break;
-
-							default:
-								throw new NotImplementedException($"{platformInfo.Platform}");
-						}
+						sb.WriteLine("throw new Exception($\"{typeof(TValue).Name} does not loaded.\");");
 					}
+					var keyName = "Key".ChangeNotation(Notation.Pascal, setting.ScriptPublicVariableNameNotation);
+					sb.WriteLine($"{dataPrivateName} = data.ToDictionary(x => x.{keyName}, x => x);");
 
-					sb.WriteLine("#else");
-					sb.WriteLine("var json = string.Empty;");
-					sb.WriteLine("#endif");
+				}
+				sb.WriteLine();
+
+				using (sb.StartScope($"public static async Task {"LoadDataAsync".ToFunctionName(setting)}()"))
+				{
+					sb.WriteLine("IsLoaded = true;");
+					sb.WriteLine($"var json = await {"ReadDataAsync".ToFunctionName(setting)}<TValue>();");
 					sb.WriteLine();
 
 					sb.WriteLine("var (_ ,data) = JsonConvert.DeserializeObject<(TValue, List<TValue>)>(json);");
