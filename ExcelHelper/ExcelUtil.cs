20413ba6a465f12101af55758a94f5b84ba6d6b4
yeop12 <duq47@naver.com>
2024-06-20 20:37:28 +0900

    Excel helper 추가

---
+++ ExcelHelper/ExcelUtil.cs
@@ -0,0 +1,118 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelHelper
+{
+	internal class ExcelUtil : IDisposable
+	{
+		private Excel.Application? _excelApp;
+        private Excel.Workbook? _workbook;
+        private Excel.Worksheet? _worksheet;
+
+        public ExcelUtil(string path, string sheetName) 
+        {
+	        try
+	        {
+				_excelApp = (Excel.Application)CustomMarshal.GetActiveObject("Excel.Application");
+	        }
+	        catch
+	        {
+				_excelApp = new Excel.Application
+				{
+					Visible = true
+				};
+	        }
+            _workbook = _excelApp.Workbooks.Open(path);
+            _worksheet = _workbook.Worksheets[sheetName];
+        }
+
+        public void ActivateSheet()
+        {
+	        _workbook?.Activate();
+            _worksheet?.Activate();
+        }
+
+        public void SelectCell(int row, int column) 
+        {
+	        _workbook?.Activate();
+            _worksheet?.Activate();
+            _worksheet?.Cells[row + 1, column + 1].Select();
+        }
+
+        private static void ReleaseExcelObject(object? obj) 
+        {
+            try 
+            {
+                if (obj is not null) 
+                {
+                    Marshal.ReleaseComObject(obj);
+                }
+            }
+            finally 
+            {
+                GC.Collect();
+            }
+        }
+
+        public void Dispose()
+        {
+	        CleanUp();
+        }
+
+        private void CleanUp() 
+        {
+            ReleaseExcelObject(_worksheet);
+            _worksheet = null;
+            ReleaseExcelObject(_workbook);
+            _workbook = null;
+            ReleaseExcelObject(_excelApp);
+            _excelApp = null;
+        }
+	}
+
+	public static class CustomMarshal
+	{
+		internal const string OLEAUT32 = "oleaut32.dll";
+		internal const string OLE32 = "ole32.dll";
+
+		[SecurityCritical]
+		public static object GetActiveObject( string progID ) 
+		{
+			object obj = null;
+			Guid clsid;
+			
+			try 
+			{
+				CLSIDFromProgIDEx(progID, out clsid);
+			}
+			catch (Exception) 
+			{
+				CLSIDFromProgID(progID, out clsid);
+			}
+
+			GetActiveObject(ref clsid, IntPtr.Zero, out obj);
+			return obj;
+		}
+		
+		[DllImport(OLE32, PreserveSig = false)]
+		[ResourceExposure(ResourceScope.None)]
+		[SuppressUnmanagedCodeSecurity]
+		[SecurityCritical]
+		private static extern void CLSIDFromProgIDEx( [MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid );
+		
+		[DllImport(OLE32, PreserveSig = false)]
+		[ResourceExposure(ResourceScope.None)]
+		[SuppressUnmanagedCodeSecurity]
+		[SecurityCritical]
+		private static extern void CLSIDFromProgID( [MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid );
+		
+		[DllImport(OLEAUT32, PreserveSig = false)]
+		[ResourceExposure(ResourceScope.None)]
+		[SuppressUnmanagedCodeSecurity]
+		[SecurityCritical]
+		private static extern void GetActiveObject( ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out Object ppunk );
+
+	}
+}
---
+++ ExcelHelper/Program.cs
@@ -0,0 +1,59 @@
+﻿using ExcelHelper;
+
+#if DEBUG
+var values = Console.ReadLine()?.Split();
+#else
+var values = args;
+#endif
+
+if (values is null || values.Length < 1)
+{
+	return;
+}
+
+if (Enum.TryParse(values[0], out Command command) is false)
+{
+	return;
+}
+
+switch (command)
+{
+	case Command.OpenSheet:
+	{
+		if (values.Length < 3)
+		{
+			return;
+		}
+
+		var path = values[1];
+		var sheetName = values[2];
+		using var sheetUtil = new ExcelUtil(path, sheetName);
+		sheetUtil.ActivateSheet();
+	}
+		break;
+
+	case Command.SelectCell:
+	{
+		if (values.Length < 5)
+		{
+			return;
+		}
+
+		var path = values[1];
+		var sheetName = values[2];
+		var row = int.Parse(values[3]);
+		var column = int.Parse(values[4]);
+		using var sheetUtil = new ExcelUtil(path, sheetName);
+		sheetUtil.SelectCell(row, column);
+	}
+		break;
+
+	default:
+		throw new NotImplementedException();
+}
+
+internal enum Command
+{
+	OpenSheet,
+	SelectCell,
+}
